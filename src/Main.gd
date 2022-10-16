cd9a3868d8236b595e4def81d9fa289dcb65b0f8
Juan Gaspar García <24842781+PKGaspi@users.noreply.github.com>
2022-10-16 17:47:28 +0200

    Minor update changes to godot 4

--- src/Main.gd
+++ src/Main.gd
@@ -4,4 +4,4 @@ extends Node
 @onready var _hp_bar = $UI/HPBar
 
 func _ready():
-	_hp_bar.to_monitor = _player
+    _hp_bar.to_monitor = _player
--- src/Player.gd
+++ src/Player.gd
@@ -13,46 +13,46 @@ var hp = 3
 
 
 func _physics_process(delta):
-	# Rotar el jugador para que mire al cursor.
-	rotation = global_position.direction_to(get_global_mouse_position()).angle()
-	
-	# Calcular la dirección de movimiento.
-	var input_dir = Vector2(
-		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	).normalized()
-	
-	# Mover.
-	set_velocity(input_dir * SPEED)
-	move_and_slide()
+    # Rotar el jugador para que mire al cursor.
+    rotation = global_position.direction_to(get_global_mouse_position()).angle()
+    
+    # Calcular la dirección de movimiento.
+    var input_dir = Vector2(
+        Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+        Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+    ).normalized()
+    
+    # Mover.
+    set_velocity(input_dir * SPEED)
+    move_and_slide()
 
 
 func _unhandled_input(event):
-	if event.is_action_pressed("shoot"):
-		shoot() # Disparar.
+    if event.is_action_pressed("shoot"):
+        shoot() # Disparar.
 
 
 func shoot():
-	# Crear nueva instancia.
-	var bullet = BULLET.instantiate() 
-	# Dar propiedades a la nueva instancia de la bala.
-	bullet.rotation = rotation
-	bullet.position = position
-	# Añadir la bala como hijo del nodo padre.
-	get_parent().add_child(bullet)
+    # Crear nueva instancia.
+    var bullet = BULLET.instantiate() 
+    # Dar propiedades a la nueva instancia de la bala.
+    bullet.rotation = rotation
+    bullet.position = position
+    # Añadir la bala como hijo del nodo padre.
+    get_parent().add_child(bullet)
 
 
 func take_damage():
-	# Comprobar si la nave es invencible.
-	if _invencibility_timer.is_stopped():
-		# No es invencible, recibir daño.
-		hp -= 1
-		# Iniciar temporizador de invencibilidad.
-		_invencibility_timer.start()
-		# Comprobar si me muero.
-		if hp <= 0:
-			die() # Me muero.
+    # Comprobar si la nave es invencible.
+    if _invencibility_timer.is_stopped():
+        # No es invencible, recibir daño.
+        hp -= 1
+        # Iniciar temporizador de invencibilidad.
+        _invencibility_timer.start()
+        # Comprobar si me muero.
+        if hp <= 0:
+            die() # Me muero.
 
 
 func die():
-	queue_free() # Borrar de la escena principal.
+    queue_free() # Borrar de la escena principal.
